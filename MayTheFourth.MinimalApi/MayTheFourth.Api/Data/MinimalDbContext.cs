0b336eb9451a6ba8653b079d60505b9faace49d5
jrenato1508 <jrenato1508@gmail.com>
2024-04-24 11:43:18 -0300

    Adicionando Projeto MayTheFourth.Api

---
+++ MayTheFourth.MinimalApi/MayTheFourth.Api/Data/MinimalDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFourth.Api.Data
+{
+    public class MinimalDbContext : DbContext
+    {
+        public MinimalDbContext(DbContextOptions<MinimalDbContext> options) : base(options){}
+
+        #region DbSet das entidades
+
+        #endregion
+
+        #region Mapeamento das entidades
+
+        #endregion
+    }
+}
---
+++ src/MayTheFourth.Api/Program.cs
@@ -0,0 +1,91 @@
+
+using Microsoft.OpenApi.Models;
+
+
+#region Configure Services
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "May The Fourth Minimal Api",
+        Description = "Desenvolvido Pela equipe composta por José Renato, Felipe André, Vinicius Baum e Matheus ",
+        License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+    }); ; ;
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o token JWT desta maneira: Bearer + espaço + seu token",
+        Name = "Authorization",
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+#endregion
+
+
+#region Configure Pipeline
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+AdicionandoActionOrEndpoint(app);
+app.Run();
+
+#endregion
+
+
+#region Actions/EndPoints
+void AdicionandoActionOrEndpoint(WebApplication app)
+{
+    #region Lista de Filmes
+
+    #endregion
+
+    #region Lista de Personagens
+
+    #endregion
+
+    #region Lista de Planetas
+
+    #endregion
+
+    #region  Lista de Veiculos
+
+    #endregion
+
+    #region Lista de Naves
+
+    #endregion
+}
+#endregion
+
+
+
